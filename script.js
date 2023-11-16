d2d7377422d3babd6b542f9d51f5a976164cb2aa
Dani2202 <daniyalq2202@gmail.com>
2023-11-16 21:00:52 +0500

    javascript added

---
+++ script.js
@@ -0,0 +1,17 @@
+const videoElement = document.getElementById('video');
+const button  = document.getElementById('button');
+
+
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObjec = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error){
+        console.log('Error:',error)
+    }
+} 
+
+selectMediaStream()
